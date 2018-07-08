c694ecef30a91c666d678245180c4f95c36f2a43
Rory <roryskipper123@gmail.com>
2018-07-07 20:23:29 -0600

    Removed signature variable from applyCustomHead method

--- src/rorys/library/util/ItemUtil.java
+++ src/rorys/library/util/ItemUtil.java
@@ -73,28 +73,27 @@ public class ItemUtil {
 
             if (mat == Material.SKULL_ITEM && this.plugin.getConfig().isSet(path + "skin-value")) {
                 item.setDurability((short) 3);
-                ItemStack oldItem = item;
-                item = this.applyCustomHead(item, this.plugin.getConfig().getString(path + "skin-value"));
-                if (item == null) {
-                    item = oldItem;
+                ItemMeta oldItemMeta = itemMeta;
+                itemMeta = this.applyCustomHead(itemMeta, this.plugin.getConfig().getString(path + "skin-value"));
+                if (itemMeta == null) {
+                    itemMeta = oldItemMeta;
                     this.plugin.getLogger().info("[" + this.plugin.getName() + "] Failed to load skull skin-value @ " + path);
                 }
             }
 
             item.setItemMeta(itemMeta);
+
             return item;
         } catch (IllegalArgumentException e) {
             return null;
         }
     }
 
-    public ItemStack applyCustomHead(ItemStack skull, String value) {
-        String signature = "H116D5fhmj/7BVWqiRQilXmvoJO6wJzXH4Dvou6P2o9YMb+HaJT8s9+zt03GMYTipzK+NsW2D2JfzagnxLUTuiOtrCHm6V2udOM0HG0JeL4zR0Wn5oHmu+S7kUPUbt7HVlKaRXry5bobFQ06nUf7hOV3kPfpUJsfMajfabmoJ9RGMRVot3uQszjKOHQjxyAjfHP2rjeI/SktBrSscx0DzwBW9LCra7g/6Cp7/xPQTIZsqz2Otgp6i2h3YpXJPy02j4pIk0H4biR3CaU7FB0V4/D1Hvjd08giRvUpqF0a1w9rbpIWIH5GTUP8eLFdG/9SnHqMCQrTj4KkQiN0GdBO18JvJS/40LTn3ZLag5LBIa7AyyGus27N3wdIccvToQ6kHHRVpW7cUSXjircg3LOsSQbJmfLoVJ/KAF/m+de4PxIjOJIcbiOkVyQfMQltPg26VzRiu3F0qRvJNAAydH8AHdaqhkpSf6yjHqPU3p3BHFJld5o59WoD4WNkE3wOC//aTpV/f9RJ0JQko08v2mGBVKx7tpN7vHD1qD5ILzV1nDCV1/qbKgiOK9QmdXqZw9J3pM/DHtZ6eiRKni9BuGWlbWFN/qfFO2xY+J7SYFqTxBbffmvwvuF83QP5UdRTNVLYoV5S+yR5ac7fVWUZmLbq7tawyuCu0Dw24M9E1BSnpSc=";
-
+    public SkullMeta applyCustomHead(ItemMeta itemMeta, String value) {
         GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
-        gameProfile.getProperties().put("textures", new Property("textures", value, signature));
+        gameProfile.getProperties().put("textures", new Property("textures", value));
 
-        SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+        SkullMeta skullMeta = (SkullMeta) itemMeta;
         try {
             Field profileField = skullMeta.getClass().getDeclaredField("profile");
             profileField.setAccessible(true);
@@ -103,8 +102,7 @@ public class ItemUtil {
             return null;
         }
 
-        skull.setItemMeta(skullMeta);
-        return skull;
+        return skullMeta;
     }
 
     public int getSlot(String path) {
