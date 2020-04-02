08def44a2fb4768224fd1fbb6dc689963cea6c17
7rory768 <roryskipper123@gmail.com>
2020-04-01 22:06:45 -0600

    Added update notifier

---
+++ src/main/java/rorys/library/util/UpdateNotifier.java
@@ -0,0 +1,67 @@
+package rorys.library.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class UpdateNotifier {
+
+	//Constants. Customize to your liking.
+	private static final String        ERR_MSG     = "&cUpdate checker failed!";
+	private final        JavaPlugin    javaPlugin;
+	private final        MessagingUtil messagingUtil;
+	private              String        UPDATE_MSG  = "";
+	private              int           ID; //The ID of your resource. Can be found in the resource URL.
+	private              String        localPluginVersion;
+	private              String        spigotPluginVersion;
+	private              boolean       needsUpdate = false;
+
+	public UpdateNotifier(final JavaPlugin javaPlugin, MessagingUtil messagingUtil, int ID) {
+		this.javaPlugin = javaPlugin;
+		this.messagingUtil = messagingUtil;
+		this.ID = ID;
+		this.localPluginVersion = javaPlugin.getDescription().getVersion();
+	}
+
+	public String getUpdateMsg() {
+		return UPDATE_MSG;
+	}
+
+	public boolean needsUpdate() {
+		return needsUpdate;
+	}
+
+	public void checkForUpdate() {
+		//The request is executed asynchronously as to not block the main thread.
+		Bukkit.getScheduler().runTaskAsynchronously(this.javaPlugin, () -> {
+			//Request the current version of your plugin on SpigotMC.
+			try {
+				HttpsURLConnection connection = (HttpsURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=" + ID).openConnection();
+				connection.setRequestMethod("GET");
+				this.spigotPluginVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+				int spaceIndex = this.spigotPluginVersion.indexOf(" ");
+				if (spaceIndex > -1) {
+					this.spigotPluginVersion = this.spigotPluginVersion.substring(0, spaceIndex);
+				}
+			} catch (IOException e) {
+				Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', ERR_MSG));
+				e.printStackTrace();
+				return;
+			}
+
+			//Check if the requested version is the same as the one in your plugin.yml.
+			if (this.localPluginVersion.equals(this.spigotPluginVersion)) return;
+
+			this.needsUpdate = true;
+			UPDATE_MSG = messagingUtil.placeholders("{PREFIX}Update available! Go to " + messagingUtil.getFirstColor() + "https://www.spigotmc.org/resources/" + ID + "/ &7to update\n"
+					+ "{PREFIX}Current version: " + messagingUtil.getFirstColor() + localPluginVersion + "\n"
+					+ "{PREFIX}New version: " + messagingUtil.getFirstColor() + spigotPluginVersion);
+		});
+	}
+}
