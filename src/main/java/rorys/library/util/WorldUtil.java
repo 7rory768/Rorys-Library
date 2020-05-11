847255d4b48dac91c922b4d10b67cb990f78cdc6
7rory768 <roryskipper123@gmail.com>
2020-05-11 15:36:53 -0600

    Added WorldUtil

---
+++ src/main/java/rorys/library/util/WorldUtil.java
@@ -0,0 +1,33 @@
+package main.java.rorys.library.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import java.io.File;
+import java.nio.file.Files;
+
+public class WorldUtil {
+
+    public static void deleteWorld(String name) {
+        File file = new File(Bukkit.getWorldContainer().getPath() + File.separator + name);
+        deleteDir(file);
+    }
+
+    public static void deleteWorld(World world) {
+        File file = new File(Bukkit.getWorldContainer().getPath() + File.separator + world.getName());
+        deleteDir(file);
+    }
+
+    public static void deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                if (!Files.isSymbolicLink(f.toPath())) {
+                    deleteDir(f);
+                }
+            }
+        }
+        file.delete();
+    }
+
+}
