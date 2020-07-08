3d03fe88994eda514f93c39bc5018c2a236e8b4a
7rory768 <roryskipper123@gmail.com>
2020-07-07 22:50:15 -0700

    The skin-value key may now be supplied with a playername, uuid, custom head link, or custom head value

--- src/main/java/roryslibrary/util/ItemUtil.java
+++ src/main/java/roryslibrary/util/ItemUtil.java
@@ -1,4 +1,4 @@
-package rorys.library.util;
+package roryslibrary.util;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
@@ -26,28 +26,28 @@ import java.util.UUID;
  * Created by Rory on 6/30/2017.
  */
 public class ItemUtil {
-
+	
 	private JavaPlugin plugin;
-
+	
 	public ItemUtil(JavaPlugin plugin) {
 		this.plugin = plugin;
 	}
-
+	
 	public ItemStack getItemStack(String path) {
 		return ItemUtil.getItemStack(this.plugin.getConfig(), path);
 	}
-
+	
 	public static ItemStack getItemStack(JavaPlugin plugin, String path) {
 		return getItemStack(plugin.getConfig(), path);
 	}
-
+	
 	public static ItemStack getItemStack(FileConfiguration config, String path) {
 		ItemStack item;
 		if (!path.endsWith(".")) {
 			path += ".";
 		}
 		Material mat;
-		String   matString = config.getString(path + "material", "NULL").toUpperCase();
+		String matString = config.getString(path + "material", "NULL").toUpperCase();
 		try {
 			mat = Material.valueOf(matString);
 		} catch (IllegalArgumentException e) {
@@ -65,19 +65,19 @@ public class ItemUtil {
 		int amount = config.getInt(path + "amount", 1);
 		item = new ItemStack(mat, amount);
 		short data = (short) config.getInt(path + "data", item.getDurability());
-
+		
 		// Allow compatibility for < 1.13 configs
-		if (matString.equals("GRAY_STAINED_GLASS_PANE")) {
+		if (matString.equalsIgnoreCase("GRAY_STAINED_GLASS_PANE")) {
 			data = (short) 7;
 		}
-
+		
 		item.setDurability(data);
 		ItemMeta itemMeta = item.getItemMeta();
-		String   name     = config.getString(path + "name", "");
+		String name = config.getString(path + "name", "");
 		if (!name.equals("")) {
 			itemMeta.setDisplayName(MessagingUtil.format(name));
 		}
-
+		
 		List<String> loreLines = config.getStringList(path + "lore");
 		if (!loreLines.isEmpty()) {
 			List<String> lore = new ArrayList<>();
@@ -86,22 +86,22 @@ public class ItemUtil {
 			}
 			itemMeta.setLore(lore);
 		}
-
+		
 		List<String> enchants = config.getStringList(path + "enchants");
 		for (String enchantInfo : enchants) {
-			int         colonIndex  = enchantInfo.indexOf(":");
+			int colonIndex = enchantInfo.indexOf(":");
 			Enchantment enchantment = Enchantment.getByName(enchantInfo.substring(0, colonIndex));
 			if (enchantment != null) {
 				int level = Integer.parseInt(enchantInfo.substring(colonIndex + 1, enchantInfo.length()));
 				itemMeta.addEnchant(enchantment, level, true);
 			}
 		}
-
+		
 		List<String> itemFlags = config.getStringList(path + "item-flags");
 		for (String itemFlag : itemFlags) {
 			itemMeta.addItemFlags(ItemFlag.valueOf(itemFlag));
 		}
-
+		
 		if ((mat.name().equals("PLAYER_HEAD") || mat.name().equals("SKULL_ITEM"))) {
 			if (mat.name().equals("SKULL_ITEM") && !config.isSet(path + "data")) {
 				item.setDurability((short) 3);
@@ -116,22 +116,22 @@ public class ItemUtil {
 				}
 			}
 		}
-
+		
 		if ((mat == Material.LEATHER_BOOTS || mat == Material.LEATHER_CHESTPLATE || mat == Material.LEATHER_HELMET || mat == Material.LEATHER_LEGGINGS) && config.isSet(path + "color")) {
 			LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) itemMeta;
 			leatherArmorMeta.setColor(Color.fromRGB(config.getInt(path + "color")));
 			itemMeta = leatherArmorMeta;
 		}
-
+		
 		item.setItemMeta(itemMeta);
-
+		
 		return item;
 	}
-
+	
 	public static void storeItemStack(ItemStack item, JavaPlugin plugin, String path) {
 		storeItemStack(item, plugin.getConfig(), path);
 	}
-
+	
 	public static void storeItemStack(ItemStack item, FileConfiguration config, String path) {
 		if (!path.endsWith(".")) {
 			path += ".";
@@ -139,17 +139,17 @@ public class ItemUtil {
 		config.set(path + "material", item.getType().name());
 		config.set(path + "amount", item.getAmount());
 		config.set(path + "data", item.getDurability());
-
+		
 		ItemMeta itemMeta = item.getItemMeta();
-
+		
 		if (itemMeta.hasDisplayName()) {
 			config.set(path + "name", itemMeta.getDisplayName());
 		}
-
+		
 		if (itemMeta.hasLore()) {
 			config.set(path + "lore", itemMeta.getLore());
 		}
-
+		
 		if (itemMeta.hasEnchants()) {
 			List<String> enchants = new ArrayList<String>();
 			for (Map.Entry<Enchantment, Integer> enchant : itemMeta.getEnchants().entrySet()) {
@@ -157,7 +157,7 @@ public class ItemUtil {
 				config.set(path + "enchants", enchants);
 			}
 		}
-
+		
 		List<String> itemFlags = new ArrayList<>();
 		for (ItemFlag itemFlag : itemMeta.getItemFlags()) {
 			itemFlags.add(itemFlag.name());
@@ -165,21 +165,34 @@ public class ItemUtil {
 		if (!itemFlags.isEmpty()) {
 			config.set(path + "item-flags", itemFlags);
 		}
-
+		
 		if ((item.getType().name().equals("SKULL_ITEM") || item.getType().name().equals("PLAYER_HEAD"))) {
 			SkullMeta skullMeta = (SkullMeta) itemMeta;
 			config.set(path + "skull-owner", skullMeta.getOwner());
 		}
 	}
-
+	
 	public JavaPlugin getPlugin() {
 		return plugin;
 	}
-
+	
 	public static SkullMeta applyCustomHead(ItemMeta itemMeta, String value) {
+		try {
+			UUID uuid;
+			if (value.length() <= 16) {
+				uuid = SkinUtil.getUUIDFromName(value);
+			} else {
+				uuid = UUID.fromString(value);
+			}
+			
+			if (uuid != null) return applyCustomHead(itemMeta, uuid);
+		} catch (Exception e) {
+			// Attempt to get skin value instead
+		}
+		
 		GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
-		gameProfile.getProperties().put("textures", new Property("textures", value));
-
+		gameProfile.getProperties().put("textures", new Property("textures", SkinUtil.getSkinValue(value)));
+		
 		SkullMeta skullMeta = (SkullMeta) itemMeta;
 		try {
 			Field profileField = skullMeta.getClass().getDeclaredField("profile");
@@ -188,26 +201,25 @@ public class ItemUtil {
 		} catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException exception) {
 			return null;
 		}
-
+		
 		return skullMeta;
 	}
-
-
-
+	
+	
 	public static int getX(int slot) {
 		return slot % 9 + 1;
 	}
-
+	
 	public static int getY(int slot) {
 		return slot / 9 + 1;
 	}
-
+	
 	public static SkullMeta applyCustomHead(ItemMeta itemMeta, UUID uuid) {
 		SkullMeta skullMeta = (SkullMeta) itemMeta;
 		skullMeta.setOwner(SkinUtil.getName(uuid));
 		return skullMeta;
 	}
-
+	
 	public static boolean isSimilar(ItemStack o, ItemStack o1) {
 		try {
 			if ((o == null && o1 != null) || (o != null && o1 == null)) {
@@ -217,20 +229,20 @@ public class ItemUtil {
 				return o.isSimilar(o1);
 			}
 		} catch (Exception e) {
-
+			
 			if (o.getType() != o1.getType() || o.getDurability() != o1.getDurability()) {
 				return false;
 			}
-
+			
 			ItemMeta oMeta = o.getItemMeta(), o1Meta = o1.getItemMeta();
-
+			
 			if (oMeta.hasDisplayName() != o1Meta.hasDisplayName() || (oMeta.hasDisplayName() && !oMeta.getDisplayName().equals(o1Meta.getDisplayName())) || oMeta.hasLore() != o1Meta.hasLore() || (oMeta.hasLore() && !oMeta.getLore().equals(o1Meta.getLore()))) {
 				return false;
 			}
 		}
 		return true;
 	}
-
+	
 	public static boolean fullInventory(Inventory inventory) {
 		for (ItemStack item : inventory.getStorageContents()) {
 			if (item == null || item.getType() == Material.AIR) {
@@ -239,9 +251,9 @@ public class ItemUtil {
 		}
 		return true;
 	}
-
+	
 	public static int getInventorySpace(Inventory inventory, ItemStack item) {
-		int space    = 0;
+		int space = 0;
 		int itemSize = item.getAmount();
 		for (int invSlot = 0; invSlot < inventory.getSize(); invSlot++) {
 			ItemStack invItem = inventory.getItem(invSlot);
@@ -250,7 +262,7 @@ public class ItemUtil {
 			} else if (invItem.isSimilar(item)) {
 				space += Math.min(invItem.getMaxStackSize() - invItem.getAmount(), itemSize);
 			}
-
+			
 			if (space >= itemSize) {
 				space = itemSize;
 				break;
@@ -258,18 +270,18 @@ public class ItemUtil {
 		}
 		return space;
 	}
-
+	
 	public static boolean itemIsReal(ItemStack item) {
 		return item != null && item.getType() != Material.AIR;
 	}
-
+	
 	public static void addDurabilityGlow(ItemStack item) {
 		ItemMeta itemMeta = item.getItemMeta();
 		itemMeta.addEnchant(Enchantment.DURABILITY, 3, true);
 		itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 		item.setItemMeta(itemMeta);
 	}
-
+	
 	public static ItemStack fillPlaceholders(ItemStack item, String... placeholders) {
 		for (int i = 0; i < placeholders.length - 1; i += 2) {
 			String placeholder = placeholders[i];
@@ -281,7 +293,7 @@ public class ItemUtil {
 			}
 			placeholders[i] = placeholder;
 		}
-
+		
 		ItemMeta itemMeta = item.getItemMeta();
 		if (itemMeta.hasDisplayName()) {
 			String name = itemMeta.getDisplayName();
@@ -290,7 +302,7 @@ public class ItemUtil {
 			}
 			itemMeta.setDisplayName(MessagingUtil.format(name));
 		}
-
+		
 		if (itemMeta.hasLore()) {
 			List<String> lore = new ArrayList<>();
 			for (String line : itemMeta.getLore()) {
@@ -301,20 +313,20 @@ public class ItemUtil {
 			}
 			itemMeta.setLore(lore);
 		}
-
+		
 		item.setItemMeta(itemMeta);
 		return item;
 	}
-
+	
 	public int getSlot(String path) {
 		return ItemUtil.getSlot(this.plugin.getConfig(), path);
 	}
-
+	
 	public static int getSlot(FileConfiguration config, String path) {
 		if (!path.endsWith(".")) {
 			path += ".";
 		}
-
+		
 		int xCord = config.getInt(path + "x-cord", -1);
 		int yCord = config.getInt(path + "y-cord", -1);
 		if (xCord == -1 || yCord == -1) {
@@ -322,9 +334,9 @@ public class ItemUtil {
 		}
 		return ItemUtil.getSlot(xCord, yCord);
 	}
-
+	
 	public static int getSlot(int xCord, int yCord) {
 		return ((yCord - 1) * 9) + xCord - 1;
 	}
-
+	
 }
