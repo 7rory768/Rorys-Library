156e0e2fdb0586fbb49ae250e8b0ae1b52993ea6
7rory768 <roryskipper123@gmail.com>
2020-08-18 18:19:55 -0600

    Added enabled check to SQLManager

--- src/main/java/roryslibrary/managers/SQLManager.java
+++ src/main/java/roryslibrary/managers/SQLManager.java
@@ -1,5 +1,6 @@
 package roryslibrary.managers;
 
+import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -9,94 +10,95 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLManager {
-
-    private final JavaPlugin plugin;
-
-    private Connection connection;
-    private String host, database, tableName, username, password;
-    private int port;
-
-    public SQLManager(JavaPlugin plugin, String tableName) {
-        this.plugin = plugin;
-        this.tableName = tableName;
-
-        FileConfiguration config = plugin.getConfig();
-        host = config.getString("mysql.host", "localhost");
-        port = config.getInt("mysql.port", 3306);
-        database = config.getString("mysql.database", "mcserver");
-        username = config.getString("mysql.username", "root");
-        password = config.getString("mysql.password", "root");
-
-        try {
-            checkConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // Keep SQL Connection active
-        (new BukkitRunnable() {
-            @Override
-            public void run() {
-                try {
-                    if (connection != null && !connection.isClosed()) {
-                        connection.createStatement().execute("SELECT 1");
-                    }
-                } catch (SQLException e) {
-                    connection = createConnection();
-                }
-            }
-        }).runTaskTimerAsynchronously(plugin, 60 * 20, 60 * 20);
-
-    }
-    
-    public Connection getConnection() {
-        return connection;
-    }
-    
-    public String getTableName() {
-        return tableName;
-    }
-    
-    public Connection createConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
-            Connection connection = DriverManager.getConnection(url, username, password);
-            return connection;
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public boolean checkConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
-            connection = createConnection();
-            if (connection == null || connection.isClosed()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void clearTable() {
-        try {
-            connection.createStatement().execute("TRUNCATE TABLE " + tableName);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public boolean execute(String sql) throws SQLException {
-        boolean success = connection.createStatement().execute(sql);
-        return success;
-    }
-
-    public void close() throws SQLException {
-        if (connection != null) {
-            connection.close();
-        }
-    }
-
+	
+	private final JavaPlugin plugin;
+	
+	@Getter
+	private Connection connection;
+	@Getter
+	private boolean enabled;
+	private String host, database, tableName, username, password;
+	private int port;
+	
+	public SQLManager(JavaPlugin plugin, String tableName) {
+		this.plugin = plugin;
+		this.tableName = tableName;
+		
+		FileConfiguration config = plugin.getConfig();
+		enabled = config.getBoolean("mysql.enabled", false);
+		host = config.getString("mysql.host", "localhost");
+		port = config.getInt("mysql.port", 3306);
+		database = config.getString("mysql.database", "mcserver");
+		username = config.getString("mysql.username", "root");
+		password = config.getString("mysql.password", "root");
+		
+		if (enabled) {
+			try {
+				checkConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			// Keep SQL Connection active
+			(new BukkitRunnable() {
+				@Override
+				public void run() {
+					try {
+						if (connection != null && !connection.isClosed()) {
+							connection.createStatement().execute("SELECT 1");
+						}
+					} catch (SQLException e) {
+						connection = createConnection();
+					}
+				}
+			}).runTaskTimerAsynchronously(plugin, 60 * 20, 60 * 20);
+		}
+		
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	
+	public Connection createConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+			Connection connection = DriverManager.getConnection(url, username, password);
+			return connection;
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public boolean checkConnection() throws SQLException {
+		if (connection == null || connection.isClosed()) {
+			connection = createConnection();
+			if (connection == null || connection.isClosed()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void clearTable() {
+		try {
+			connection.createStatement().execute("TRUNCATE TABLE " + tableName);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean execute(String sql) throws SQLException {
+		boolean success = connection.createStatement().execute(sql);
+		return success;
+	}
+	
+	public void close() throws SQLException {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+	
 }
