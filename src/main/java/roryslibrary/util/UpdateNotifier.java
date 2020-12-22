26cfd83b853b1c14dbd6cb1249fd05171a06f821
Rory Skipper <rory@ripplegroup.ca>
2020-12-21 18:58:36 -0700

    No longer falsely reports that an update (while spigot cache hasn't be updated yet)

--- src/main/java/roryslibrary/util/UpdateNotifier.java
+++ src/main/java/roryslibrary/util/UpdateNotifier.java
@@ -11,32 +11,32 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class UpdateNotifier {
-
+	
 	//Constants. Customize to your liking.
-	private static final String        ERR_MSG     = "&cUpdate checker failed!";
-	private final        JavaPlugin    javaPlugin;
-	private final        MessagingUtil messagingUtil;
-	private              String        UPDATE_MSG  = "";
-	private              int           ID; //The ID of your resource. Can be found in the resource URL.
-	private              String        localPluginVersion;
-	private              String        spigotPluginVersion;
-	private              boolean       needsUpdate = false;
-
+	private static final String ERR_MSG = "&cUpdate checker failed!";
+	private final JavaPlugin javaPlugin;
+	private final MessagingUtil messagingUtil;
+	private String UPDATE_MSG = "";
+	private int ID; //The ID of your resource. Can be found in the resource URL.
+	private String localPluginVersion;
+	private String spigotPluginVersion;
+	private boolean needsUpdate = false;
+	
 	public UpdateNotifier(final JavaPlugin javaPlugin, MessagingUtil messagingUtil, int ID) {
 		this.javaPlugin = javaPlugin;
 		this.messagingUtil = messagingUtil;
 		this.ID = ID;
 		this.localPluginVersion = javaPlugin.getDescription().getVersion();
 	}
-
+	
 	public String getUpdateMsg() {
 		return UPDATE_MSG;
 	}
-
+	
 	public boolean needsUpdate() {
 		return needsUpdate;
 	}
-
+	
 	public void checkForUpdate() {
 		//The request is executed asynchronously as to not block the main thread.
 		Bukkit.getScheduler().runTaskAsynchronously(this.javaPlugin, () -> {
@@ -54,11 +54,24 @@ public class UpdateNotifier {
 				e.printStackTrace();
 				return;
 			}
-
+			
 			//Check if the requested version is the same as the one in your plugin.yml.
 			if (this.localPluginVersion.equals(this.spigotPluginVersion)) return;
-
-			this.needsUpdate = true;
+			
+			String[] localNums = localPluginVersion.split(".");
+			String[] spigotNums = spigotPluginVersion.split(".");
+			boolean needsUpdate = false;
+			for (int i = 0; i < Math.max(localNums.length, spigotNums.length); i++) {
+				int localNum = localNums.length <= i ? 0 : Integer.parseInt(localNums[i]);
+				int spigotNum = spigotNums.length <= i ? 0 : Integer.parseInt(spigotNums[i]);
+				if (localNum > spigotNum) {
+					return;
+				} else if (localNum < spigotNum) {
+					needsUpdate = true;
+					break;
+				}
+			}
+			
 			UPDATE_MSG = messagingUtil.placeholders("{PREFIX}Update available! Go to " + messagingUtil.getFirstColor() + "https://www.spigotmc.org/resources/" + ID + "/ &7to update\n"
 					+ "{PREFIX}Current version: " + messagingUtil.getFirstColor() + localPluginVersion + "\n"
 					+ "{PREFIX}New version: " + messagingUtil.getFirstColor() + spigotPluginVersion);
