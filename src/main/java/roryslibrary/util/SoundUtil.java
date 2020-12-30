a7f3e0f83aa0d96412aabb388ca50416369aadce
Rory Skipper <rory@ripplegroup.ca>
2020-12-29 19:45:49 -0700

    Added getSoundInfo method

--- src/main/java/roryslibrary/util/SoundUtil.java
+++ src/main/java/roryslibrary/util/SoundUtil.java
@@ -4,9 +4,10 @@ import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import roryslibrary.sounds.SoundInfo;
 
 public class SoundUtil {
-
+	
 	public static void playSound(Player player, JavaPlugin plugin, String path) {
 		playSound(player, plugin.getConfig(), path);
 	}
@@ -15,7 +16,7 @@ public class SoundUtil {
 		if (!path.endsWith(".")) {
 			path += ".";
 		}
-
+		
 		Sound sound = null;
 		String soundString = config.getString(path + "sound").toUpperCase();
 		try {
@@ -30,6 +31,32 @@ public class SoundUtil {
 			}
 		}
 		player.playSound(player.getLocation(), sound, Float.valueOf(config.getString(path + "volume", "1.0")), Float.valueOf(config.getString(path + "pitch", "1.0")));
+	}
+	
+	public static SoundInfo getSoundInfo(JavaPlugin plugin, String path) {
+		return getSoundInfo(plugin.getConfig(), path);
+	}
+	
+	public static SoundInfo getSoundInfo(FileConfiguration config, String path) {
+		if (!path.endsWith(".")) path += ".";
+		
+		Sound sound = null;
+		String soundString = config.getString(path + "sound").toUpperCase();
+		try {
+			sound = Sound.valueOf(soundString);
+		} catch (IllegalArgumentException e) {
+			if (soundString.equals("BLOCK_NOTE_BLOCK_PLING")) sound = Sound.valueOf("NOTE_PLING");
+			else if (soundString.equals("ENTITY_PLAYER_LEVELUP")) sound = Sound.valueOf("LEVEL_UP");
+			else if (soundString.equals("BLOCK_ANVIL_PLACE")) sound = Sound.valueOf("ANVIL_LAND");
+		}
+		
+		if (sound != null) {
+			float volume = Float.valueOf(config.getString(path + "volume", "1.0"));
+			float pitch = Float.valueOf(config.getString(path + "pitch", "1.0"));
+			return new SoundInfo(sound, volume, pitch);
+		}
+		
+		return null;
 	}
 	
 }
