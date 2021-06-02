ec437626b56e093ae07f6279fb8f69c448199e0c
Rory Skipper <roryskipper123@gmail.com>
2021-06-01 20:26:30 -0600

    Added method to load SoundInfo from ConfigurationSection

--- src/main/java/roryslibrary/util/SoundUtil.java
+++ src/main/java/roryslibrary/util/SoundUtil.java
@@ -1,6 +1,8 @@
 package roryslibrary.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Sound;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -8,10 +10,12 @@ import roryslibrary.sounds.SoundInfo;
 
 public class SoundUtil {
 	
+	@Deprecated
 	public static void playSound(Player player, JavaPlugin plugin, String path) {
 		playSound(player, plugin.getConfig(), path);
 	}
 	
+	@Deprecated
 	public static void playSound(Player player, FileConfiguration config, String path) {
 		if (!path.endsWith(".")) {
 			path += ".";
@@ -38,21 +42,26 @@ public class SoundUtil {
 	}
 	
 	public static SoundInfo getSoundInfo(FileConfiguration config, String path) {
-		if (!path.endsWith(".")) path += ".";
-		
+		if (path.endsWith(".")) path = path.substring(0, path.length() - 1);
+		return getSoundInfo(config.getConfigurationSection(path));
+	}
+	
+	public static SoundInfo getSoundInfo(ConfigurationSection section) {
 		Sound sound = null;
-		String soundString = config.getString(path + "sound").toUpperCase();
+		String soundString = section.getString("sound").toUpperCase();
 		try {
 			sound = Sound.valueOf(soundString);
 		} catch (IllegalArgumentException e) {
 			if (soundString.equals("BLOCK_NOTE_BLOCK_PLING")) sound = Sound.valueOf("NOTE_PLING");
 			else if (soundString.equals("ENTITY_PLAYER_LEVELUP")) sound = Sound.valueOf("LEVEL_UP");
 			else if (soundString.equals("BLOCK_ANVIL_PLACE")) sound = Sound.valueOf("ANVIL_LAND");
+			else
+				Bukkit.getLogger().severe("[SoundUtil] Failed to load sound `" + soundString + "` @ " + section.getCurrentPath());
 		}
 		
 		if (sound != null) {
-			float volume = Float.valueOf(config.getString(path + "volume", "1.0"));
-			float pitch = Float.valueOf(config.getString(path + "pitch", "1.0"));
+			float volume = Float.valueOf(section.getString("volume", "1.0"));
+			float pitch = Float.valueOf(section.getString("pitch", "1.0"));
 			return new SoundInfo(sound, volume, pitch);
 		}
 		
